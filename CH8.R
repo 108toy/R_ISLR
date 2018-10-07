22303a67700e37fe25d3da3e88542474e4691076
Deng <ddeng@baf.local>
2018-10-07 15:01:23 +0800

    Finish 8.3.2, moving to 8.3.3

--- CH8.R
+++ CH8.R
@@ -14,9 +14,10 @@ tree.carseats <- tree(High~.-Sales, Carseats)
 
 summary(tree.carseats)
 ##### DD: results
-# small deviance indeicates a good fit to the (traning)
+# small deviance indicates a good fit to the (traning)
 # residual mean deviance reported is simply the deviance divided by n−|T0|
-# n: 
+# n: number of samples
+# |T0|: number of nodes
 
 plot(tree.carseats)
 text(tree.carseats)
@@ -73,3 +74,38 @@ table(tree.pred, High.test)
 
 
 ##### 8.3.2 Fitting Regression Trees ##### 
+
+library(MASS)
+set.seed(1)
+train <- sample(1:nrow(Boston), nrow(Boston)/2)
+tree.boston <- tree(medv~., Boston, subset = train)
+summary(tree.boston)
+
+plot(tree.boston)
+text(tree.boston , pretty = 0)
+
+#### Cross validation
+
+cv.boston=cv.tree(tree.boston)
+plot(cv.boston$size ,cv.boston$dev ,type='b')
+
+# the most complex tree is the best
+# but if we still wait to prune the tree we can:
+
+prune.boston=prune.tree(tree.boston ,best=5)
+plot(prune.boston)
+text(prune.boston , pretty =0)
+
+# then the prediction
+
+yhat=predict (tree.boston ,newdata=Boston[-train ,])
+boston.test=Boston[-train ,"medv"]
+plot(yhat ,boston.test)
+abline (0,1)
+mean((yhat -boston.test)^2)
+
+####
+
+
+##### 8.3.3 Bagging and Random Forests #####
+
