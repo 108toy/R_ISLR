b00aac2ad51d95aa2cb7faf5a71376f7ca7ff9ef
108toy <dengyi.danny@gmail.com>
2018-10-18 17:42:43 +0800

    9.6.3 ROC Curves

--- CH9.R
+++ CH9.R
@@ -71,4 +71,35 @@ plot(svmfit , dat)
 
 #### 9.6.2 Support Vector Machine
 
+# to fit an SVM using a non-linear kernel
 
+# to fit an SVM with a polynomial kernel use kernel="polynomial", se the degree argument to specify a degree for the polynomial kernel (this is d in (9.22)),
+# to fit an SVM with a radial kernel we use kernel="radial".  u and in the latter case we use gamma to specify a value of γ for the radial basis kernel (9.24)
+
+set.seed(1)
+x = matrix(rnorm (200*2) , ncol = 2)
+x[1:100,] = x[1:100,]+2
+x[101:150 ,] = x[101:150,] - 2
+y = c(rep(1,150) ,rep(2,50))
+dat = data.frame(x = x,y = as.factor(y))
+
+plot(x, col=y) # Non linear
+
+train = sample(200,100)
+svmfit = svm(y~., data = dat[train ,], kernel = "radial", gamma = 1, cost = 1)
+plot(svmfit , dat[train ,])
+
+summary(svmfit)
+
+# tune the model:
+set.seed(1)
+tune.out = tune(svm , y~., data=dat[train ,], kernel ="radial",
+                ranges=list(cost=c(0.1,1,10,100,1000),
+                            gamma=c(0.5,1,2,3,4) ))
+summary(tune.out)
+
+table(true=dat[-train ,"y"], pred=predict (tune.out$best.model ,
+                                           newdata =dat[-train ,]))
+
+
+#### 9.6.3 ROC Curves
