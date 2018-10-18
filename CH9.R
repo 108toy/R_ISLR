1907cdb9d81933ce1b72a881d51da09a885d0cf5
108toy <dengyi.danny@gmail.com>
2018-10-18 19:24:03 +0800

    9.6.5 Application to Gene Expression Data

--- CH9.R
+++ CH9.R
@@ -129,3 +129,20 @@ fitted = attributes (predict(svmfit.flex ,dat[- train ,], decision.values=T))$de
 rocplot(fitted ,dat[-train ,"y"],add=T,col="red")
 
 # 9.6.4 SVM with Multiple Classes
+# the svm() function will perform multi-class classification using the one-versus-one approach
+
+set.seed(1)
+x <- rbind(x, matrix(rnorm (50*2) , ncol=2))
+y <- c(y, rep(0,50))
+x[y == 0, 2] <- x[y == 0, 2] + 2
+dat <- data.frame(x = x, y = as.factor(y))
+par(mfrow=c(1,1))
+plot(x,col=(y+1))
+
+svmfit = svm(y~., data=dat , kernel ="radial", cost=10, gamma =1) # it will deal with the multi-class itself
+plot(svmfit , dat)
+
+# 9.6.5 Application to Gene Expression Data
+
+
+
